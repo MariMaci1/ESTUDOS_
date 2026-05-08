8d888fc2eb19ab274b0dbfb60464cba1beec6cf7
MariMaci1 <estudosdamari973@gmail.com>
2026-05-08 19:36:21 -0300

    Create contador.js

---
+++ contador.js
@@ -0,0 +1,15 @@
+//contador crescente 1 -> 5
+
+let contador = 1;
+while(contador <=5){
+  console.log(contador)
+  contador++
+}
+
+//contador decrescente 5 -> 1
+
+let contador = 5;
+while(contador >=1){
+  console.log(contador)
+  contador--
+}

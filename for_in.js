37207cc36a94d91427320ca21cdd58a3920c2566
MariMaci1 <estudosdamari973@gmail.com>
2026-05-11 19:13:36 -0300

    Add attributes section in for_in.js

---
+++ for_in.js
@@ -0,0 +1,14 @@
+//Atributos
+
+const pessoa1 = {
+    nome: "Mariana",
+    telefone: "(21)0000-0000",
+    endereco: "ABC"
+}
+
+const carros = ['Gol' , 'fusca' , 'ka']
+
+console.log(pessoa1);
+
+for(let atributo in pessoa1){
+    console.log(atributo)

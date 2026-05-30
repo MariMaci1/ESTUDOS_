64ff8fa17c1d3515615b200fd3005a441f467d1b
Mariana Maciel <estudosdamari973@gmail.com>
2026-05-30 01:51:50 -0300

    Initialize alunos array and collect user notes

---
+++ arrays.js
@@ -0,0 +1,26 @@
+// Arrays
+
+alunos = [
+    [0,8,9,4],
+    [1,2,5,8],
+    [3,6,9,[6,4]]
+];
+
+console.table(alunos)
+console.table(alunos[2][3][0])
+
+
+// Arrays
+
+alunos = [];
+qtdNota = 0
+let qtd = prompt("Digite a quantidade de notas: ");
+
+for(let i=1; i<=qtd; i++){
+    qtdNota <= qtd
+    qtdNota++
+    let nota = prompt(`Digite a ${qtdNota} nota:`)
+    alunos.push(nota)
+}
+
+console.log(alunos)
